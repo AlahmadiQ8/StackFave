9ea172b609da56abfde5d29aace69f83a8f4d60e
AlahmadiQ8 <m91.alahmadi@gmail.com>
2018-04-17 01:42:34 -0700

    Bang Bang

---
+++ config/background.js
@@ -0,0 +1,67 @@
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status !== 'loading') {
+    return;
+  }
+
+  const cssFile = 'main.css';
+  const jsFile = 'main.js';
+
+  const cb = res => {
+    if (chrome.runtime.lastError) {
+      return;
+    }
+  };
+
+  chrome.tabs.insertCSS(tabId, { file: cssFile, runAt: 'document_start' }, cb);
+  chrome.tabs.executeScript(
+    tabId,
+    {
+      file: jsFile,
+      runAt: 'document_start',
+    },
+    cb
+  );
+});
+
+// chrome.runtime.onMessage.addListener((req, sender, sendRes) => {
+//   const handler = {
+//     requestPermissions: () => {
+//       const urls = (req.urls || [])
+//         .filter(url => url.trim() !== '')
+//         .map(url => {
+//           if (url.slice(-2) === '/*') return url;
+//           if (url.slice(-1) === '/') return url + '*';
+//           return url + '/*';
+//         });
+
+//       if (urls.length === 0) {
+//         sendRes(true);
+//         removeUnnecessaryPermissions();
+//       } else {
+//         chrome.permissions.request({ origins: urls }, granted => {
+//           sendRes(granted);
+//           removeUnnecessaryPermissions();
+//         });
+//       }
+//       return true;
+
+//       function removeUnnecessaryPermissions() {
+//         const whitelist = urls.concat([
+//           'https://github.com/*',
+//           'https://bitbucket.org/*',
+//         ]);
+//         chrome.permissions.getAll(permissions => {
+//           const toBeRemovedUrls = permissions.origins.filter(url => {
+//             return !~whitelist.indexOf(url);
+//           });
+
+//           if (toBeRemovedUrls.length) {
+//             chrome.permissions.remove({ origins: toBeRemovedUrls });
+//           }
+//         });
+//       }
+//     },
+//   };
+
+//   return handler[req.type]();
+// });
---
+++ replace_bundle.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cssFilePath="$(find ./dist -name 'main*.css')"
+cssFile="${cssFilePath##*/}"
+jsFilePath="$(find ./dist -name 'main*.js')"
+jsFile="${jsFilePath##*/}"
+sed -e "s/main.*\.js/${jsFile}/" dist/background.js > tmp && mv tmp dist/background.js
