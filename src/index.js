3bd53ad2052886b6739fcf55ce4e7758769c6784
AlahmadiQ8 <m91.alahmadi@gmail.com>
2018-04-22 17:04:09 -0700

    toggle localstorage on show/hide sidebar

--- src/index.js
+++ src/index.js
@@ -4,42 +4,50 @@ import App from './App';
 import { Native as Store } from './utils/store';
 import { DEFAULTS, STORE } from './constants';
 import './index.css';
-import { exec } from 'child_process';
 
 let $html;
 let $body;
 let $root;
+const store = new Store();
 
 ready(() => {
   $html = document.documentElement;
   $body = document.body;
-  $html.classList.add('ext-html-show');
   $root = document.createElement('div');
   $root.setAttribute('id', 'root');
   $root.setAttribute('class', 'ext_sidebar');
   $body.appendChild($root);
+
+  Object.keys(STORE).forEach(key => {
+    store.setDefault(STORE[key], DEFAULTS[key]);
+  });
+  $html.classList.add('ext-html-show');
+  if (store.get(STORE.SHOW_SIDEBAR)) {
+    showSideBar();
+  } else {
+    hideSideBar();
+  }
+
   ReactDOM.render(<AppContainer />, document.getElementById('root'));
 });
 
 class AppContainer extends Component {
   constructor(props) {
     super(props);
-    this.store = new Store();
-    Object.keys(STORE).forEach(key => {
-      this.store.setDefault(STORE[key], DEFAULTS[key]);
-    });
     this.state = {
-      open: false,
+      open: store.get(STORE.SHOW_SIDEBAR),
     };
   }
 
   toggleOpen = e => {
     e.preventDefault();
     this.setState(({ open }) => ({ open: !open }));
-    $root.classList.toggle('ext_sidebar-hide');
-    $html.classList.toggle('ext-html-show');
-    $html.classList.toggle('ext_html-hide');
+    toggleSideBar();
   };
+
+  componentDidUpdate() {
+    store.set(STORE.SHOW_SIDEBAR, this.state.open);
+  }
 
   render() {
     return (
@@ -47,10 +55,10 @@ class AppContainer extends Component {
         <button
           onClick={this.toggleOpen}
           className={`ext_toggle-btn ${
-            this.state.open ? 'ext_toggle-btn-hide' : ''
+            !this.state.open ? 'ext_toggle-btn-hide' : ''
           }`}
         >
-          {this.state.open ? '>' : '<'}
+          {this.state.open ? '<' : '>'}
         </button>
         <App />
       </React.Fragment>
@@ -68,4 +76,20 @@ function ready(fn) {
   } else {
     document.addEventListener('DOMContentLoaded', fn);
   }
+}
+
+function showSideBar() {
+  $root.classList.remove('ext_sidebar-hide');
+  $html.classList.add('ext-html-show');
+  $html.classList.remove('ext_html-hide');
+}
+function hideSideBar() {
+  $root.classList.add('ext_sidebar-hide');
+  $html.classList.remove('ext-html-show');
+  $html.classList.add('ext_html-hide');
+}
+function toggleSideBar() {
+  $root.classList.toggle('ext_sidebar-hide');
+  $html.classList.toggle('ext-html-show');
+  $html.classList.toggle('ext_html-hide');
 }
