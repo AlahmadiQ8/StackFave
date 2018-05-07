c35cbbc556b2abac04712425fb1f368367950e7d
AlahmadiQ8 <m91.alahmadi@gmail.com>
2018-05-07 01:26:42 -0700

    added custom filter for questions

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,18 +1,22 @@
 const baseURL = 'https://api.stackexchange.com/2.2';
-const site = 'stackoverflow';
 const key = '1WP26yIS*3APRVewJHDjww((';
+
+// generated from createFilter method
+const filter = '!0XXAMzZV3)6nNHjQ18538kAUL';
 
 export default class Api {
   constructor(token) {
     this.token = token;
   }
 
-  _buildURL(endpoint, queries) {
-    const baseQuery = `?key=${key}&access_token=${this.token}&site=${site}`;
-    const url = `${baseURL}${endpoint}${baseQuery}`;
-    if (queries) {
-      return url + `&${queries}`;
-    }
+  _buildURL(endpoint, queriesObj, site) {
+    const query = `?${this._objToQuery({
+      key,
+      access_token: this.token,
+      ...(site && { site }),
+      ...queriesObj,
+    })}`;
+    const url = `${baseURL}${endpoint}${query}`;
     return url;
   }
 
@@ -24,17 +28,65 @@ export default class Api {
     return entries.map(([key, val]) => `${key}=${val}`).join('&');
   }
 
-  async _fetch(endpoint, queriesObj = {}) {
-    const url = this._buildURL(endpoint, this._objToQuery(queriesObj));
+  async _fetch(endpoint, queriesObj = {}, site = 'stackoverflow') {
+    const url = this._buildURL(endpoint, queriesObj, site);
     return fetch(url).then(res => {
       return res.json();
     });
   }
 
+  // used during development to generate filter
+  async _createFilter() {
+    const includeFeilds = [
+      'question.title',
+      'question.creation_date',
+      'question.view_count',
+      'question.favorite_count',
+      'question.up_vote_count',
+      'question.tags',
+      'question.link',
+    ];
+    const excludeFeilds = [
+      'question.accepted_answer_id',
+      'question.answer_count',
+      'question.bounty_amount',
+      'question.bounty_closes_date',
+      'question.closed_date',
+      'question.closed_reason',
+      'question.community_owned_date',
+      'question.is_answered',
+      'question.last_activity_date',
+      'question.last_edit_date',
+      'question.locked_date',
+      'question.migrated_from',
+      'question.migrated_to',
+      'question.owner',
+      'question.protected_date',
+      'question.question_id',
+      'question.score',
+    ];
+    const { items } = await this._fetch(
+      '/filters/create',
+      {
+        include: includeFeilds.join(';'),
+        exclude: excludeFeilds.join(';'),
+        base: 'default',
+        unsafe: false,
+      },
+      ''
+    );
+    if (items.length === 0) {
+      return '';
+    }
+    console.log(items);
+    const filterName = items[0].filter;
+    return filterName;
+  }
+
   async getFavorites(queriesObj = {}) {
     let favorites = [];
     let hasMore = false;
-    const mergedQuery = Object.assign({ page: 1 }, queriesObj);
+    const mergedQuery = Object.assign({ page: 1, filter }, queriesObj);
     do {
       if (hasMore) {
         mergedQuery.page += 1;
