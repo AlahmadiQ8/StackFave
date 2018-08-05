da79529b7b97b00aefc59bc2d915aae8d5034eac
AlahmadiQ8 <m91.alahmadi@gmail.com>
2018-08-04 19:36:41 -0700

    :arrow_up: bump react-select to stable release v2

--- dist/background.js
+++ dist/background.js
@@ -5,7 +5,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   }
 
   const cssFile = 'static/css/main.edf5e42e.css';
-  const jsFile = 'static/js/main.b758575c.js';
+  const jsFile = 'static/js/main.8fd7161e.js';
 
   const cb = res => {
     if (chrome.runtime.lastError) {
